cea3f149320e8bd4cd8b1e6b177dff18394e467d
Ray Kao <raykao@gmail.com>
2023-01-16 13:20:37 -0500

    fix: can only work if have already deployed Cog Services via portal to accept terms

--- infra/terraform/iot-edge-devices/azurerm-cognitive-account.tf
+++ infra/terraform/iot-edge-devices/azurerm-cognitive-account.tf
@@ -1,20 +1,10 @@
-# resource "azurerm_role_assignment" "cognitive-services" {
-#   scope                = data.azurerm_subscription.current.id
-#   role_definition_name = "Cognitive Services Contributor"
-#   principal_id         = data.azurerm_client_config.current.object_id
-# }
-
 # resource "azurerm_cognitive_account" "default" {
-#     depends_on = [
-#       azurerm_role_assignment.cognitive-services
-#     ]
-
 #   name                = local.name
 #   location            = azurerm_resource_group.default.location
 #   resource_group_name = azurerm_resource_group.default.name
 #   kind                = "TextAnalytics"
 
-#   sku_name = "S0"
+#   sku_name = "S"
 
 #   custom_question_answering_search_service_id = azurerm_search_service.default.id
 #   custom_question_answering_search_service_key = azurerm_search_service.default.primary_key
@@ -22,7 +12,7 @@
 
 
 # resource "azurerm_search_service" "default" {
-#   name                = "default-search-service"
+#   name                = local.name
 #   resource_group_name = azurerm_resource_group.default.name
 #   location            = azurerm_resource_group.default.location
 #   sku                 = "standard"
