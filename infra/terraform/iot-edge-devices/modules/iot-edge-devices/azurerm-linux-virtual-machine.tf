4c4d56f88296b3d669f482654f92cd123e57dea3
Ray Kao <raykao@gmail.com>
2023-01-15 22:53:58 -0500

    add: Linux VMs will now create per iot-edge-device

---
+++ infra/terraform/iot-edge-devices/modules/iot-edge-devices/azurerm-linux-virtual-machine.tf
@@ -0,0 +1,40 @@
+
+resource "azurerm_network_interface" "example" {
+  name                = "${local.device_id}-nic"
+  location            = var.resource_group.location
+  resource_group_name = var.resource_group.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = var.subnet_id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "example" {
+  name                = local.device_id
+  resource_group_name = var.resource_group.name
+  location            = var.resource_group.location
+  size                = "Standard_D2s_v3"
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts-gen2"
+    version   = "20.04.202301130"
+  }
+}
